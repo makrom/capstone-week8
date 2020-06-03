17585bec707a408e45c2466e28c7eabce34fcad4
makrom <makrom61@gmail.com>
2020-06-03 18:09:57 +0300

    prepare to production

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,4 +1,5 @@
 Devise.setup do |config|
   #config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
   config.navigational_formats = [:json]
+  config.secret_key = 'fbeb223e2dd888374cdb3bff12b7c3c88fe73eb2b836d716f68e34b2c50ccb761abce03b9bbce077bac88c546d60bb96d5529165a9a582d534525f21a1309c92'
 end
